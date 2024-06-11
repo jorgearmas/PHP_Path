6cf63e7967cf113a695cbeebbf99ef5daf5df431
jarmas <jorgearmasc0496@gmail.com>
2024-06-10 23:46:35 -0600

    1 - outputs and variable declaration

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+
+//Data output
+    echo 'Hello',' ','guys';
+
+//Data output
+    print(" Hello");
+
+/* The difference between echo and print is that print returns value (1). */
+
+//Escaping quotes
+    echo " Joe's invoice";
+
+//Variables
+    /* Rules:
+        - Variables must start with dollar a sign
+        - Variables must start with a letter or underscore
+        - Variables cannot be named 'this'*/
+
+    $name = ' Joe ';
+    print($name);
+
+//Variable assigment by reference and not by value using '&'
+
+    $x = 3;
+    $y = &$x;
+    $x = 5;
+
+    print($y);
+    //'y' will take whatever value “x” takes regardless of the allocation time.
+
+//Concat
+    //With echo using double quotes
+    echo " Hello {$name} ";
+    echo "Hello ".$name;
+
+    //With print
+    print("Hello ".$name);
+?>
+
+<!-- PHP in HTML -->
+<!DOCTYPE html>
+<html lang="en">
+<body>
+    <h1>
+        <?php
+            $p1 = 'This is a';
+            $p2 = 'title';
+            echo $p1.' '.$p2;
+        ?>
+    </h1>
+</body>
+</html>
