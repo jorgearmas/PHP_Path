3b1dfa4de7dc017847d568aca0ee27035845d06d
jarmas <jorgearmasc0496@gmail.com>
2024-06-13 09:05:13 -0600

    HL added

--- index.php
+++ index.php
@@ -58,7 +58,7 @@
         <li><a href="./_07_string_data_types.php">String data type</a></li>
         <li><a href="./_08_null_data_types.php">Null data type</a></li>
         <li><a href="./_09_array_data_types.php">Array data type</a></li>
-
+        <li><a href="./_10_expressions.php">Expressions</a></li>
     </ul>
 </body>
 </html>
