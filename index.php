d312dbacbe582284621ce29fb551a67b45d446bd
jarmas <jorgearmasc0496@gmail.com>
2024-06-19 10:30:56 -0600

    HL added

--- index.php
+++ index.php
@@ -67,6 +67,7 @@
         <li><a href="./_16_return_declare_tickable_stmts.php">Return, declare and tickable statements</a></li>
         <li><a href="./_17_include_files.php">Include files</a></li>
         <li><a href="./_18_functions.php">Functions</a></li>
+        <li><a href="./_19_named_args_varadic_functions.php">Named arguments and variadic functions</a></li>
     </ul>
 </body>
 </html>
